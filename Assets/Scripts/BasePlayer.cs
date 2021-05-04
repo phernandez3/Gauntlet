8a6cdd52efbe9a4d9b89fd831edfdb815223aac4
Paul Hernandez <phernandez3@mail.csuchico.edu>
2021-05-04 12:35:43 -0700

    Base player

---
+++ Assets/Scripts/BasePlayer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasePlayer : MonoBehaviour
+{
+    // Player-controller stuff
+    public int controllerSlot;
+
+    // Basic stats
+    public int score;
+    public int health;
+    public int keys;
+    public int bombs;
+
+    // Per class stats
+    public int meleePower;
+    public int rangePower;
+    public float rangeSpeed;
+    public GameObject bulletPrefab;
+    public int armor;
+    public float moveSpeed;
+    public int magic;
+
+
+}
