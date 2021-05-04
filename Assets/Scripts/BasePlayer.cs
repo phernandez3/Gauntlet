e7aa6c3bc6db0fbb1d145912105338129d924429
Paul Hernandez <phernandez3@mail.csuchico.edu>
2021-05-04 13:48:20 -0700

    Update BasePlayer.cs

--- Assets/Scripts/BasePlayer.cs
+++ Assets/Scripts/BasePlayer.cs
@@ -2,25 +2,55 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BasePlayer : MonoBehaviour
+public class BasePlayer : BaseUnit
 {
+    // Unit
+    public int health;
+    public float moveSpeed;
+
     // Player-controller stuff
     public int controllerSlot;
+    public string MoveX;
+    public string MoveY;
+    public string FireX;
+    public string FireY;
+    public string Bomb;
 
-    // Basic stats
+    // Basic player stats
     public int score;
-    public int health;
     public int keys;
     public int bombs;
 
-    // Per class stats
+    // Per player class stats
     public int meleePower;
     public int rangePower;
     public float rangeSpeed;
     public GameObject bulletPrefab;
     public int armor;
-    public float moveSpeed;
+
     public int magic;
+
+
+    // Move
+    // MeleeAttack
+    // RangedAttack
+    // TakeDamage(int)
+    // Die
+
+    // PlayerInputs
+    // Bomb
+    // Lose HP over time
+
+    
+    public void InitInputs()
+    {
+        MoveX = "MoveX" + controllerSlot;
+        MoveY = "MoveY" + controllerSlot;
+        FireX = "FireX" + controllerSlot;
+        FireY = "FireY" + controllerSlot;
+        Bomb = "Bomb" + controllerSlot;
+    }
+
 
 
 }
